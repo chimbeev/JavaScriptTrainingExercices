ff346ae978cd6cc89130fba3bdeb5b6c3fa8d360
Chimbeev <chimbeevaa@gmail.com>
2022-08-24 13:50:27 +0800

    24082022

---
+++ Exercise7_1.js
@@ -0,0 +1,43 @@
+//Напишите функцию getInfo(persons), которая принимает массив объектов, описывающих человека, имеющих поля {name, age}, 
+//и возвращает объект со средним возрастом и именем самого старшего человека.
+
+
+let arrHumans = [];
+
+arrHumans[0] = {
+    name: 'Andrey',
+    age: 18
+};
+arrHumans[1] = {
+    name: 'Sergey',
+    age: 23
+};
+arrHumans[2] = {
+    name: 'Olya',
+    age: 38
+};
+arrHumans[3] = {
+    name: 'Tanya',
+    age: 21
+};
+
+function getInfo(persons) {
+    let middleAge;
+    const sum = persons.reduce(function(acc, obj) {return acc + obj.age; }, 0);//находим сумму значений age
+    const length = persons.length; 
+    middleAge = sum / length; //вычисляем средний возраст
+    
+
+    const max = persons.reduce(function(acc, obj) { return (acc > obj.age ? acc : obj.age) }, 0);//находим максимум из значений age
+    
+    const nameOld = persons.find(person => person.age === max).name; //находим имя самого старшего
+
+    const result = {
+        name: 
+        age: max;
+    };
+
+    return middleAge, max;
+}
+
+console.log(`Средний возраст ${getInfo(arrHumans)}`);
