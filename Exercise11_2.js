dbaf3c347354408ee1ee6a643ddb5ba6f4b7d4df
Chimbeev <chimbeevaa@gmail.com>
2022-09-01 17:45:15 +0800

    01092022

--- Exercise11_2.js
+++ Exercise11_2.js
@@ -1,5 +1,12 @@
 //Создайте раскрывающийся блок (accordion). Сверху блок с заголовком, при нажатии на который снизу показывается
 // блок с текстом, при повторном нажатии блок с текстом скрывается.
+const initAccordion = element => {
+    const panel = element.querySelector('.panel');
+    const header = element.querySelector('.header');
+    header.addEventListener('click', () => {
+        panel.classList.toggle('panel--opened');
+    });
+}
+
 const accordions = document.querySelectorAll('.accordion');
-
-
+accordions.forEach(initAccordion);
